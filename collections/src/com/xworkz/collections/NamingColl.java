e0325058b2adf76136b217d0e7381d0135b03e18
darshan2429 <darshannb.xworkz@gmail.com>
2022-09-14 22:56:34 +0530

    collections 140922

---
+++ collections/src/com/xworkz/collections/NamingColl.java
@@ -0,0 +1,39 @@
+package com.xworkz.collections;
+
+import java.util.ArrayList;
+
+public class NamingColl {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("Kawasaki Z900");
+		list.add("Ducati Multistrada");
+		list.add("BMW GSA 1250");
+		list.add("Himalayan");
+		list.add("Aprilia Tuono V4");
+		
+		System.out.println(list);
+		
+		System.out.println(list.remove(2));
+		
+		System.out.println(list.lastIndexOf(2));
+		
+		System.out.println(list.clone());
+		
+		System.out.println(list.indexOf(3));
+		
+		System.out.println(list.add("VW Polo"));
+		
+		System.out.println(list);
+		
+		System.out.println(list.get(2));
+
+		System.out.println(list.isEmpty());
+		
+		list.clear();
+		
+	}
+
+}
