9a9da3e5c0bdb941a9ebd95f045f01f942b547d9
darshan2429 <darshannb.xworkz@gmail.com>
2022-10-10 23:42:41 +0530

    sorting 101022

--- collection/src/com/xworkz/collection/achiever/HouseAchiever.java
+++ collection/src/com/xworkz/collection/achiever/HouseAchiever.java
@@ -10,7 +10,7 @@ import com.xworkz.collection.enums.Type;
 public class HouseAchiever {
 
 	public static void main(String[] args) {
-		
+
 		HouseDTO houseDTO1 = new HouseDTO(1, "Prestige", "Chandru", Type.APARTMENT, 18, true, Owning.LEASE, 2000);
 		HouseDTO houseDTO2 = new HouseDTO(2, "Salarpuria", "Prajwal", Type.APARTMENT, 15, true, Owning.OWN, 1800);
 		HouseDTO houseDTO3 = new HouseDTO(3, "Sobha", "Sachin", Type.INDIVIDUAL, 35, false, Owning.RENT, 3000);
@@ -20,20 +20,23 @@ public class HouseAchiever {
 		HouseDTO houseDTO7 = new HouseDTO(7, "Brigade", "Prajwal", Type.APARTMENT, 15, true, Owning.OWN, 1800);
 		HouseDTO houseDTO8 = new HouseDTO(8, "Kings & Queen", "Sachin", Type.INDIVIDUAL, 35, false, Owning.LEASE, 3000);
 		HouseDTO houseDTO9 = new HouseDTO(9, "Pheonix", "Venkatesh", Type.VILLA, 20, true, Owning.RENT, 2200);
-		HouseDTO houseDTO10 = new HouseDTO(10, "Ideal Homes", "Sudarshan", Type.INDIVIDUAL, 16, false, Owning.OWN, 1700);
+		HouseDTO houseDTO10 = new HouseDTO(10, "Ideal Homes", "Sudarshan", Type.INDIVIDUAL, 16, false, Owning.OWN,
+				1700);
 		HouseDTO houseDTO11 = new HouseDTO(11, "Sun Shine", "Chandru", Type.APARTMENT, 18, true, Owning.LEASE, 2000);
 		HouseDTO houseDTO12 = new HouseDTO(12, "Creekside", "Prajwal", Type.APARTMENT, 15, true, Owning.OWN, 1800);
-		HouseDTO houseDTO13 = new HouseDTO(13, "Lakeside habitat", "Sachin", Type.INDIVIDUAL, 35, false, Owning.LEASE, 3000);
+		HouseDTO houseDTO13 = new HouseDTO(13, "Lakeside habitat", "Sachin", Type.INDIVIDUAL, 35, false, Owning.LEASE,
+				3000);
 		HouseDTO houseDTO14 = new HouseDTO(14, "Green Space", "Venkatesh", Type.VILLA, 20, true, Owning.RENT, 2200);
 		HouseDTO houseDTO15 = new HouseDTO(15, "Oaks", "Sudarshan", Type.INDIVIDUAL, 16, false, Owning.OWN, 1700);
 		HouseDTO houseDTO16 = new HouseDTO(16, "Big tree", "Chandru", Type.APARTMENT, 18, true, Owning.LEASE, 2000);
 		HouseDTO houseDTO17 = new HouseDTO(17, "Garden square", "Prajwal", Type.APARTMENT, 15, true, Owning.OWN, 1800);
 		HouseDTO houseDTO18 = new HouseDTO(18, "The Nest", "Sachin", Type.INDIVIDUAL, 35, false, Owning.LEASE, 3000);
 		HouseDTO houseDTO19 = new HouseDTO(19, "Blue Sky", "Venkatesh", Type.VILLA, 20, true, Owning.RENT, 2200);
-		HouseDTO houseDTO20 = new HouseDTO(20, "Ocean Shores", "Sudarshan", Type.INDIVIDUAL, 16, false, Owning.OWN, 1700);
-		
+		HouseDTO houseDTO20 = new HouseDTO(20, "Ocean Shores", "Sudarshan", Type.INDIVIDUAL, 16, false, Owning.OWN,
+				1700);
+
 		List<HouseDTO> houseDTOs = new ArrayList<HouseDTO>();
-		
+
 		houseDTOs.add(houseDTO5);
 		houseDTOs.add(houseDTO3);
 		houseDTOs.add(houseDTO2);
@@ -54,57 +57,60 @@ public class HouseAchiever {
 		houseDTOs.add(houseDTO19);
 		houseDTOs.add(houseDTO18);
 		houseDTOs.add(houseDTO20);
-		
+
 		System.out.println("=====Display Id asce=====");
-		houseDTOs.stream().sorted((s1,s2) -> s1.getId().compareTo(s2.getId())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().sorted((s1, s2) -> s1.getId().compareTo(s2.getId())).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by Id desc=====");
-		houseDTOs.stream().sorted((s1,s2) -> s2.getId().compareTo(s1.getId())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().sorted((s1, s2) -> s2.getId().compareTo(s1.getId())).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by name asce=====");
-		houseDTOs.stream().sorted((s1,s2) -> s1.getName().compareTo(s2.getName())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().sorted((s1, s2) -> s1.getName().compareTo(s2.getName()))
+				.forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by type desc=====");
-		houseDTOs.stream().sorted((s1,s2) -> s2.getType().compareTo(s1.getType())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().sorted((s1, s2) -> s2.getType().compareTo(s1.getType()))
+				.forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by sqfeet desc=====");
-		houseDTOs.stream().sorted((s1,s2) -> s2.getSqFeet().compareTo(s1.getSqFeet())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().sorted((s1, s2) -> s2.getSqFeet().compareTo(s1.getSqFeet()))
+				.forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by owner desc ");
-		houseDTOs.stream().filter((s)->s.getLoan()==true).sorted((s1,s2)-> s2.getLoan().compareTo(s1.getLoan())).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().filter((s) -> s.getLoan() == true).sorted((s1, s2) -> s2.getLoan().compareTo(s1.getLoan()))
+				.forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by owner desc 2 ");
-		houseDTOs.stream().filter((s)->s.getLoan()==false).sorted((s1,s2)-> s2.getLoan().compareTo(s1.getLoan())).forEach((s)->System.out.println(s));
-	
+		houseDTOs.stream().filter((s) -> s.getLoan() == false).sorted((s1, s2) -> s2.getLoan().compareTo(s1.getLoan()))
+				.forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by lease=====");
-		houseDTOs.stream().filter((s)->s.getOwningType()==Owning.LEASE).forEach((s)->System.out.println(s));
-	
+		houseDTOs.stream().filter((s) -> s.getOwningType() == Owning.LEASE).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by Apartment=====");
-		houseDTOs.stream().filter((s)->s.getType()==Type.APARTMENT).forEach((s)->System.out.println(s));
-	
+		houseDTOs.stream().filter((s) -> s.getType() == Type.APARTMENT).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display by Villa=====");
-		houseDTOs.stream().filter((s)->s.getType()==Type.VILLA).forEach((s)->System.out.println(s));
-		
+		houseDTOs.stream().filter((s) -> s.getType() == Type.VILLA).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display type and owningtype sorting asc=====");
-		houseDTOs.stream().sorted((s1,s2)->{
+		houseDTOs.stream().sorted((s1, s2) -> {
 			int c = s1.getType().compareTo(s2.getType());
-			if(c==0) 
-			{
+			if (c == 0) {
 				return s1.getOwningType().compareTo(s2.getOwningType());
 			}
 			return c;
-		}).forEach((s)->System.out.println(s));
-		
+		}).forEach((s) -> System.out.println(s));
+
 		System.out.println("=====Display name and id sorting desc=====");
-		houseDTOs.stream().sorted((s1,s2)->{
+		houseDTOs.stream().sorted((s1, s2) -> {
 			int s = s2.getName().compareTo(s1.getName());
-			if(s==0) {
+			if (s == 0) {
 				return s2.getId().compareTo(s1.getId());
 			}
 			return 0;
-		}).forEach((s)->System.out.println(s));
-	
-	
+		}).forEach((s) -> System.out.println(s));
+
 	}
 
 }
--- collection/src/com/xworkz/collection/achiever/PubAchiever.java
+++ collection/src/com/xworkz/collection/achiever/PubAchiever.java
@@ -8,15 +8,15 @@ import com.xworkz.collection.dto.PubDTO;
 public class PubAchiever {
 
 	public static void main(String[] args) {
-		
-		PubDTO pubDTO1 = new PubDTO(1,"Lono Tropical Lounge", "Indiranagar", "good", 1400, true);
-		PubDTO pubDTO2 = new PubDTO(2,"Taproom", "JP Nagar", "good", 1200, true);
-		PubDTO pubDTO3 = new PubDTO(3,"Toit", "Indiranagar", "normal", 1500, true);
-		PubDTO pubDTO4 = new PubDTO(4,"Gustoes", "Jayanagar", "Better", 1400, true);
-		PubDTO pubDTO5 = new PubDTO(5,"Fat owl", "JP Nagar", "Good", 1600, true);
-		
+
+		PubDTO pubDTO1 = new PubDTO(1, "Lono Tropical Lounge", "Indiranagar", "good", 1400, true);
+		PubDTO pubDTO2 = new PubDTO(2, "Taproom", "JP Nagar", "good", 1200, true);
+		PubDTO pubDTO3 = new PubDTO(3, "Toit", "Indiranagar", "normal", 1500, true);
+		PubDTO pubDTO4 = new PubDTO(4, "Gustoes", "Jayanagar", "Better", 1400, true);
+		PubDTO pubDTO5 = new PubDTO(5, "Fat owl", "JP Nagar", "Good", 1600, true);
+
 		List<PubDTO> pubDTOs = new ArrayList<PubDTO>();
-		
+
 		pubDTOs.add(pubDTO4);
 		pubDTOs.add(pubDTO3);
 		pubDTOs.add(pubDTO1);
@@ -24,19 +24,20 @@ public class PubAchiever {
 		pubDTOs.add(pubDTO2);
 
 		System.out.println("-----display id in asce");
-		pubDTOs.stream().sorted((c1,c2)->c1.getId().compareTo(c2.getId())).forEach((e)->System.out.println(e));
-		
+		pubDTOs.stream().sorted((c1, c2) -> c1.getId().compareTo(c2.getId())).forEach((e) -> System.out.println(e));
+
 		System.out.println("-----display id in asce");
-		pubDTOs.stream().filter((c)->c.getArea()!=null).sorted((c1,c2)->c1.getName().compareTo(c2.getName())).forEach((e)->System.out.println(e));
-		
+		pubDTOs.stream().filter((c) -> c.getArea() != null).sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
+				.forEach((e) -> System.out.println(e));
+
 		System.out.println("=====Display area and name sorting desc=====");
-		pubDTOs.stream().sorted((c1,c2)->{
+		pubDTOs.stream().sorted((c1, c2) -> {
 			int v = c1.getArea().compareTo(c2.getArea());
-			if(v==0) {
+			if (v == 0) {
 				return c1.getName().compareTo(c2.getName());
 			}
 			return v;
-		}).forEach((e)->System.out.println(e));
+		}).forEach((e) -> System.out.println(e));
 	}
 
 }
